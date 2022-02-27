9574b10db42fd7e1622da9eb2a61b558f9b51cdf
jasonjayoo <94942855+jasonjayoo@users.noreply.github.com>
2022-02-27 12:21:21 -0800

    added descriptions to Intern Test

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -4,43 +4,43 @@ const Engineer = require("../lib/Engineer");
 // creates engineer object
 describe("Engineer", () => {
   describe("Object", () => {
-it("should create an Engineer object", () => {
-  const engineer = new Engineer(
-    "Jason Yoo",
-    222,
-    "jasonjayoo@outlook.com",
-    "jasonjayoo"
-  );
+    it("should create an Engineer object", () => {
+      const engineer = new Engineer(
+        "Jason Yoo",
+        222,
+        "jasonjayoo@outlook.com",
+        "jasonjayoo"
+      );
 
-  expect(engineer.github).toEqual(expect.any(String));
-});
-});
-// gets github from getGithub()
-describe("Github", () => {
-it("should get engineer github value", () => {
-  const engineer = new Engineer(
-    "Jason Yoo",
-    222,
-    "jasonjayoo@outlook.com",
-    "jasonjayoo"
-  );
+      expect(engineer.github).toEqual(expect.any(String));
+    });
+  });
+  // gets github from getGithub()
+  describe("Github", () => {
+    it("should get engineer github value", () => {
+      const engineer = new Engineer(
+        "Jason Yoo",
+        222,
+        "jasonjayoo@outlook.com",
+        "jasonjayoo"
+      );
 
-  expect(engineer.getGithub()).toEqual(
-    expect.stringContaining(engineer.github.toString())
-  );
-});
-});
-// gets role from getRole()
-describe("Role", () => {
-it("should get role of employee", () => {
-  const engineer = new Engineer(
-    "Jason Yoo",
-    222,
-    "jasonjayoo@outlook.com",
-    "jasonjayoo"
-  );
+      expect(engineer.getGithub()).toEqual(
+        expect.stringContaining(engineer.github.toString())
+      );
+    });
+  });
+  // gets role from getRole()
+  describe("Role", () => {
+    it("should get role of employee", () => {
+      const engineer = new Engineer(
+        "Jason Yoo",
+        222,
+        "jasonjayoo@outlook.com",
+        "jasonjayoo"
+      );
 
-  expect(engineer.getRole()).toEqual("Engineer");
-});
-});
+      expect(engineer.getRole()).toEqual("Engineer");
+    });
+  });
 });
--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -2,24 +2,47 @@
 const Intern = require("../lib/Intern");
 
 // creates intern object
-test("creates an Intern object", () => {
-  const intern = new Intern("Jason Yoo", 222, "jasonjayoo@outlook.com", "UCI");
+describe("Intern", () => {
+  describe("Object", () => {
+    it("should create an Intern object", () => {
+      const intern = new Intern(
+        "Jason Yoo",
+        222,
+        "jasonjayoo@outlook.com",
+        "UCI"
+      );
 
-  expect(intern.school).toEqual(expect.any(String));
-});
+      expect(intern.school).toEqual(expect.any(String));
+    });
+  });
 
-// gets school from getSchool()
-test("gets employee school", () => {
-  const intern = new Intern("Jason Yoo", 222, "jasonjayoo@outlook.com", "UCI");
+  // gets school from getSchool()
+  describe("School", () => {
+    test("gets employee school", () => {
+      const intern = new Intern(
+        "Jason Yoo",
+        222,
+        "jasonjayoo@outlook.com",
+        "UCI"
+      );
 
-  expect(intern.getSchool()).toEqual(
-    expect.stringContaining(intern.school.toString())
-  );
-});
+      expect(intern.getSchool()).toEqual(
+        expect.stringContaining(intern.school.toString())
+      );
+    });
+  });
 
-// gets role from getRole()
-test("gets role of employee", () => {
-  const intern = new Intern("Jason Yoo", 222, "jasonjayoo@outlook.com", "UCI");
+  // gets role from getRole()
+  describe("Role", () => {
+    test("gets role of employee", () => {
+      const intern = new Intern(
+        "Jason Yoo",
+        222,
+        "jasonjayoo@outlook.com",
+        "UCI"
+      );
 
-  expect(intern.getRole()).toEqual("Intern");
+      expect(intern.getRole()).toEqual("Intern");
+    });
+  });
 });
